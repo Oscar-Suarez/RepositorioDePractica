dc8bd0fe688f2251bf2b03ab97273e51e037720a
OscarDev <oscar.suarezwgd@gmail.com>
2022-10-13 16:29:10 -0500

    Se crea un repositorio demo para practicar en GitHub

---
+++ Como se ve un archivo .Java y una carpeta ZIP en GitHub/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int resultado;
+        resultado = suma(2, 2, 2);
+        System.out.println(resultado);
+    }
+
+    public static int suma(int a, int b, int c) {
+
+        return a + b + c;
+    }
+}
+
+
+
+
+
